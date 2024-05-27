7245924e2fe64f2c4d4e729c4f9a6fa324e24866
pankaj-gitcode <123591656+pankaj-gitcode@users.noreply.github.com>
2024-05-27 21:28:39 +0530

    Update otpFocus.js

--- focusShiftDir/otpFocus.js
+++ focusShiftDir/otpFocus.js
@@ -1,13 +1,43 @@
+const boxIds = ['id1','id2','id3'];
 const boxFunc = ()=>{
-  const inputIds = ['id1', 'id2', 'id3'];
-
-  return inputIds.map((elem)=>{
-    const inputBox = document.createElement('input');
+  return boxIds.map((elem)=>{
+  const inputBox = document.createElement('input');
     inputBox.setAttribute('id', elem);
     inputBox.setAttribute('maxlength', 2)
-    document.body.appendChild(inputBox);
+    document.body.appendChild(inputBox)
   })
+  
 }
-
 const box = boxFunc()
 console.log(box)
+
+const eventListener = (id)=>{
+  // elemId = document.getElementById(`${id}`);
+  document.getElementById(`${id}`).addEventListener('onchange', ()=>elemId.focus())
+}
+
+const boxElem = ()=>{
+  const maxlength = 2; 
+  const value1 = document.getElementById(`${boxIds[0]}`).value
+  const value2 = document.getElementById(`${boxIds[1]}`).value
+  const value3 = document.getElementById(`${boxIds[2]}`).value
+
+  const elemId1 = document.getElementById('id1');
+  const elemId2 = document.getElementById('id2');
+  const elemId3 = document.getElementById('id3');
+
+  // elemId1.focus();
+  if(value1.length < 2){
+    elemId1.focus()
+    elemId1.addEventListener('onkeyup', ()=>{
+      if(value2.length < 2){
+        console.log("val: ",value1)
+        return elemId2.focus()}
+    })
+    }
+  
+}
+
+
+
+boxElem()
