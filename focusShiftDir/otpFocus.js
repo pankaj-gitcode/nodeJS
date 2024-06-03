149bc65c0fe36e81d9290e1f591f438152fef843
pankaj-gitcode <123591656+pankaj-gitcode@users.noreply.github.com>
2024-06-03 22:57:27 +0530

    Update otpFocus.js
    OTP boxes, OTP digits

--- focusShiftDir/otpFocus.js
+++ focusShiftDir/otpFocus.js
@@ -1,43 +1,100 @@
-const boxIds = ['id1','id2','id3'];
-const boxFunc = ()=>{
-  return boxIds.map((elem)=>{
+const inputBoxIds = ['id1', 'id2', 'id3']
+const maxLength = 2;
+
+//creating 3 boxes
+const inputBoxes = ()=>{
+  inputBoxIds.map((inputId)=>{
   const inputBox = document.createElement('input');
-    inputBox.setAttribute('id', elem);
-    inputBox.setAttribute('maxlength', 2)
-    document.body.appendChild(inputBox)
+    inputBox.setAttribute('id', inputId);
+    inputBox.setAttribute('maxlength', maxLength);
+    document.body.appendChild(inputBox);
   })
+}
+inputBoxes();
+
+//new inputBoxes
+const inputBox1 = document.getElementById('id1');
+const inputBox2 = document.getElementById('id2');
+const inputBox3 = document.getElementById('id3');
+
+//boxes function invokes input Handler
+const inputBoxFn1 = ()=>inputHandler();
+const inputBoxFn2 = ()=>inputHandler();
+const inputBoxFn3 = ()=>inputHandler();
+
+//create focus on 1st box
+inputBox1.focus()
+
+//inputHandler to shift focus()
+const inputHandler = ()=>{
   
+  if(inputBox1.value.length < 2){
+    inputBox1.focus();
+  }
+  else if(inputBox2.value.length < maxLength){
+    inputBox2.focus();
+  }
+  else if(inputBox3.value.length < maxLength){
+    inputBox3.focus();
+  }
+  else{
+    const newPara = document.createElement('p');
+    document.body.appendChild(newPara);
+    newPara.innerText = "OTP Entered!!"
+  }
 }
-const box = boxFunc()
-console.log(box)
 
-const eventListener = (id)=>{
-  // elemId = document.getElementById(`${id}`);
-  document.getElementById(`${id}`).addEventListener('onchange', ()=>elemId.focus())
-}
 
-const boxElem = ()=>{
-  const maxlength = 2; 
-  const value1 = document.getElementById(`${boxIds[0]}`).value
-  const value2 = document.getElementById(`${boxIds[1]}`).value
-  const value3 = document.getElementById(`${boxIds[2]}`).value
+//add eventListener to each new input boxes
+inputBox1.addEventListener('input', inputBoxFn1);
+inputBox2.addEventListener('input', inputBoxFn2);
+inputBox3.addEventListener('input', inputBoxFn3);
 
-  const elemId1 = document.getElementById('id1');
-  const elemId2 = document.getElementById('id2');
-  const elemId3 = document.getElementById('id3');
-
-  // elemId1.focus();
-  if(value1.length < 2){
-    elemId1.focus()
-    elemId1.addEventListener('onkeyup', ()=>{
-      if(value2.length < 2){
-        console.log("val: ",value1)
-        return elemId2.focus()}
-    })
+//random number- OTP
+const otp = ()=>{
+  let upperCase = [];
+  let lowerCase = [];
+  let num = [];
+  let OTP = []
+  let OTPNum = [];
+  let OTPNumConcat = [];
+  
+  for(let i=0; i<91; i++){
+    if(i >= 65 && i <= 90){
+      const capsLetr = String.fromCharCode(i);
+      const smallLetr = String.fromCharCode(i).toLowerCase();
+      upperCase.push(capsLetr);
+      lowerCase.push(smallLetr);
+      OTP.push(capsLetr);
+      OTP.push(smallLetr);
     }
+    else if(i >= 0 && i <=10){
+      const numbers = i
+      num.push(i)
+      OTP.push(i);
+    }
+  }
   
+  for(let i=0; i<6; i++){
+    const OTPIndex = Math.floor(Math.random()*63);
+    OTPNum.push(OTP[OTPIndex])
+  }
+  console.log([upperCase ,lowerCase, num])
+  
+  //concatinating the OTP digits
+  for(let i=0;i<OTPNum.length ; i++){
+    if(i%2===0){
+      OTPNumConcat.push([i]+OTPNum[i+1])
+    }
+  }
+  
+  return OTP, OTPNumConcat.join()
 }
+const OTP = otp()
+console.log(OTP)
 
+// display OTP
+const para = document.createElement('h1');
+para.innerText = OTP
+document.body.appendChild(para)
 
-
-boxElem()
